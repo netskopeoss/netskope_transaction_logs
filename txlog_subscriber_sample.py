9d8ffd9f2c5fe90efd3fc625a06b8f3aa695af8e
dzhang <dzhang@netskope.com>
2022-02-03 15:59:02 -0800

    add sample codes

---
+++ txlog_subscriber_sample.py
@@ -0,0 +1,110 @@
+# Copyright 2022 Netskope Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# To use:
+# 1. export GOOGLE_APPLICATION_CREDENTIALS=/path/to/key.json
+# 2. python txlog_subscriber_sample.py {subscription_path} {timeout}
+# example: python txlog_subscriber_sample.py projects/123/locations/us-west1-a/subscriptions/my_subscription 600
+
+import argparse
+import datetime
+from google.cloud.pubsublite.types import MessageMetadata
+from google.pubsub_v1 import PubsubMessage
+import gzip
+
+# Sample Python Subscriber to Netskope Transaction Events.
+def receive_messages(
+    project_number, cloud_region, zone_id, subscription_id, timeout=90
+):
+    from concurrent.futures._base import TimeoutError
+    from google.cloud.pubsublite.cloudpubsub import SubscriberClient
+    from google.cloud.pubsublite.types import (
+        CloudRegion,
+        CloudZone,
+        FlowControlSettings,
+        SubscriptionPath,
+    )
+
+    location = CloudZone(CloudRegion(cloud_region), zone_id)
+    subscription_path = SubscriptionPath(
+        project_number, location, subscription_id)
+    per_partition_flow_control_settings = FlowControlSettings(
+        messages_outstanding=1000,
+        bytes_outstanding=10 * 1024 * 1024,
+    )
+
+    def callback(message: PubsubMessage):
+        metadata = MessageMetadata.decode(message.message_id)
+        print(
+            f"\n\nReceived msg at {datetime.datetime.now()} with partition {metadata.partition} offset {str(metadata.cursor).strip()}")
+
+        print("Atrributes:")
+        print(f"Content-Encoding: {message.attributes['Content-Encoding']}")
+        print(f"Log-Count: {message.attributes['Log-Count']}")
+        print(f"Fields: {message.attributes['Fields']}")
+        # for testing purpose, some data may not be gzip data, hence pass that case
+        try:
+            event = gzip.decompress(message.data)
+            print("Transaction event:")
+            print(f"{event.decode('utf-8')}")
+        except:
+            pass
+
+        message.ack()
+
+
+    with SubscriberClient() as subscriber_client:
+
+        streaming_pull_future = subscriber_client.subscribe(
+            subscription_path,
+            callback=callback,
+            per_partition_flow_control_settings=per_partition_flow_control_settings,
+        )
+
+        print(
+            f"Listening for transaction events on {str(subscription_path)}...")
+
+        try:
+            streaming_pull_future.result(timeout=timeout)
+        except TimeoutError or KeyboardInterrupt:
+            streaming_pull_future.cancel()
+            assert streaming_pull_future.done()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        "subscription_path", help="Transaction event subscription path")
+    parser.add_argument(
+        "timeout",
+        nargs="?",
+        default=90,
+        type=int,
+        help="Timeout in second (default to 90s)",
+    )
+
+    args = parser.parse_args()
+    parts = args.subscription_path.split("/")
+    location = parts[3].split("-")
+
+    receive_messages(
+        parts[1],
+        location[0] + '-' + location[1],
+        location[2],
+        parts[5],
+        args.timeout,
+    )
+
---
+++ txlog_subscriber_seek_target_sample.py
@@ -0,0 +1,100 @@
+# Copyright 2022 Netskope Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# To use:
+# 1. python3 txlog_subscriber_seek_target_sample.py -p <subscription_path> -t <timestamp_type> -s <timestamp>
+# 2. example: 
+#    python3 txlog_subscriber_seek_target_sample.py -p projects/123/locations/us-west1-a/subscriptions/my_subscription -t PUBLISH -s "2022-02-02 15:40:00"
+#    python3 txlog_subscriber_seek_target_sample.py -p projects/123/locations/us-west1-a/subscriptions/my_subscription -t EVENT -s "2022-02-02 15:40:00"
+#    python3 txlog_subscriber_seek_target_sample.py -p projects/123/locations/us-west1-a/subscriptions/my_subscription -t BEGIN
+#    python3 txlog_subscriber_seek_target_sample.py -p projects/123/locations/us-west1-a/subscriptions/my_subscription -t END
+
+
+import argparse
+from datetime import datetime
+from google.api_core.exceptions import NotFound
+from google.cloud.pubsublite import AdminClient
+from google.cloud.pubsublite.types import CloudRegion, CloudZone, SubscriptionPath, BacklogLocation, PublishTime, EventTime
+
+# Sample Python SeekModifier to Netskope Transaction Events.
+def move_cursor(project_number, cloud_region, zone_id, subscription_id, timestamp_type, timestamp):
+    try:
+        if timestamp_type == 'BEGIN':
+            ts = BacklogLocation.BEGINNING
+        elif timestamp_type == 'END':
+            ts = BacklogLocation.END
+        elif timestamp_type in ('PUBLISH', 'EVENT'):
+            dt = datetime.fromisoformat(timestamp)
+            ts = PublishTime(dt) if timestamp_type == 'PUBLISH' else EventTime(dt)
+        else:
+            print("invalid input timestamp, see --help")
+    except:
+        print("invalid input timestamp, see --help")
+        return
+
+    print(f"try to move seek cursor to {ts}")
+    location = CloudZone(CloudRegion(cloud_region), zone_id)
+    subscription_path = SubscriptionPath(
+        project_number, location, subscription_id)
+
+    client = AdminClient(cloud_region)
+    try:
+        # Initiate an out-of-band seek for a subscription to the specified
+        # target. If an operation is returned, the seek has been successfully
+        # registered and will eventually propagate to subscribers.
+        seek_operation = client.seek_subscription(subscription_path, ts)
+        print(f"Seek operation: {seek_operation.operation.name}")
+    except NotFound:
+        print(f"{subscription_path} not found.")
+        return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog="""
+subscription_path is the first argument
+timestamp_type is the second argument
+timestamp_type value could be:
+  - 'BEGIN': replays from the beginning of all retained messages.
+  - 'END': skips past all current published messages.
+  - 'PUBLISH': PublishTime('<datetime>'), delivers messages with publish time greater than or equal to the specified timestamp.
+  - 'EVENT': EventTime('<datetime>'), seeks to the first message with event time greater than or equal to the specified timestamp.
+timestamp is the last argument which is required if the second argument is 'PUBLISH' or 'EVENT'
+  - timestamp in format 'YYYY-MM-DD HH:MM:SS.fff, (.fff) is optional, e.g.'2011-11-04 00:05:23'
+""")
+
+    parser.add_argument(
+        '-p', '--subscription_path', required=True, help="Transaction event subscription path")
+    parser.add_argument(
+        '-t', '--timestamp_type', required=True, choices=['BEGIN', 'END', 'PUBLISH', 'EVENT'],
+        help="Transaction event Timestamp Type")
+    parser.add_argument(
+        '-s', '--timestamp', type=str,
+        help="timestamp in format 'YYYY-MM-DD HH:MM:SS.fff, (.fff) is optional, e.g.'2011-11-04 00:05:23'")
+
+    args = parser.parse_args()
+    if args.subscription_path and args.timestamp_type:
+        parts = args.subscription_path.split("/")
+        location = parts[3].split("-")
+
+        move_cursor(
+            parts[1],
+            location[0] + '-' + location[1],
+            location[2],
+            parts[5],
+            args.timestamp_type,
+            args.timestamp,
+        )
+    else:
+        parser.print_help()
